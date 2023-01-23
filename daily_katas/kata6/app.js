8d6239f17e6d22b16e0e42352108cf5dd704cbcb
samuelahw <samuel.ahjoniemi@gmail.com>
2023-01-23 10:28:44 +0200

    Daily kata 6 fix

--- daily_katas/kata6/app.js
+++ daily_katas/kata6/app.js
@@ -9,11 +9,10 @@ const countBoomerangs = (arr) => {
     return count
 }
 
-
-console.log(countBoomerangs([9, 5, 9, 5, 1, 1, 1]));
-console.log(countBoomerangs([5, 6, 6, 7, 6, 3, 9]));
-console.log(countBoomerangs([4, 4, 4, 9, 9, 9, 9]));
-console.log(countBoomerangs([1, 7, 1, 7, 1, 7, 1]));
-console.log(countBoomerangs([]));
-console.log(countBoomerangs([1, 7]));
-console.log(countBoomerangs([1, 7, 1, 7, "one", 7, 1]));
+console.log(countBoomerangs([9, 5, 9, 5, 1, 1, 1])); // 2
+console.log(countBoomerangs([5, 6, 6, 7, 6, 3, 9])); // 1
+console.log(countBoomerangs([4, 4, 4, 9, 9, 9, 9])); // 0
+console.log(countBoomerangs([1, 7, 1, 7, 1, 7, 1])); // 5
+console.log(countBoomerangs([])); // error
+console.log(countBoomerangs([1, 7])); // error
+console.log(countBoomerangs([1, 7, 1, 7, "one", 7, 1])); // error
